d2e7e7f0e7273952a7ab1f73a54e4939eedea499
blackhu0804 <812510003@qq.com>
2021-03-21 13:58:52 +0800

    feat: init project

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,38 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  <p>{{ state.name }} {{ name2 }}</p>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, reactive, computed, watchEffect } from "vue";
 import HelloWorld from "./components/HelloWorld.vue";
 
 export default defineComponent({
   name: "App",
   components: {
     HelloWorld
+  },
+  mounted() {
+    console.log(this.name2);
+  },
+  setup(props, { slots, attrs, emit }) {
+    const state = reactive({
+      name: 1
+    });
+
+    const computedNameRef = computed(() => {
+      return state.name + "2";
+    });
+
+    watchEffect(() => {
+      console.log(state.name);
+    });
+
+    return {
+      state,
+      name2: computedNameRef
+    };
   }
 });
 </script>
---
+++ src/app.tsx
@@ -0,0 +1,13 @@
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  setup() {
+    return () => {
+      return (
+        <div id="app">
+          <h1>hello vue3</h1>
+        </div>
+      );
+    };
+  }
+});
